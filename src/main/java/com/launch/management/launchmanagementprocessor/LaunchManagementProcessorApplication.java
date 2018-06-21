25105ab665ee07e9bbebae5c3de840b8bae5f26c
fabriciofelipe <portalf@gmail.com>
2018-06-21 17:22:02 -0300

    first commit

---
+++ src/main/java/com/launch/management/launchmanagementprocessor/LaunchManagementProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.launch.management.launchmanagementprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchManagementProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchManagementProcessorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/launch/management/launchmanagementprocessor/LaunchManagementProcessorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.launch.management.launchmanagementprocessor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LaunchManagementProcessorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
